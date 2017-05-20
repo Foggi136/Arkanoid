2171b119cdf529e007712de8a12380ee96f65197
Foggi136 <foggi139@gmail.com>
2017-05-20 19:12:17 +0300

    bag fix

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+    private bool ballActive;
+    private Vector3 ballPosition;
+  static  private Vector2 ballInitialForce;
+    public GameObject playerObject;
+    // Use this for initialization
+    void Start () {
+        //создать силу
+        ballInitialForce = new Vector2(100.0f, 300.0f);
+        //переход в неактивное состояние
+        ballActive = false;
+        //заполнить положение
+        ballPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //проверим нажатие на кнопку
+        if (Input.GetButtonDown("Jump") == true)
+        {
+            //проверка состояния, задаем силу только в том случае, если шар в неактивном состоянии
+            if(!ballActive)
+            {
+                //применяем саму силу
+              //  Rigidbody2D.
+                //и задаем активное состояние
+                ballActive = !ballActive;
+            }
+            if (!ballActive && playerObject != null)
+            {
+                // задаем новую позицию шарика
+                ballPosition.x = playerObject.transform.position.x;
+
+                // устанавливаем позицию шара
+                transform.position = ballPosition;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerScript: MonoBehaviour
+{
+
+    // Use this for initialization
+    public float playerVelocity;
+    public float boundary;
+    private Vector3 playerPosition;
+    void Start () {
+        playerPosition = gameObject.transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //горизонтальное двжение
+        playerPosition.x += Input.GetAxis("Horizontal") * playerVelocity;
+        // выход из игры
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        // обновим позицию платформы
+        transform.position = playerPosition;
+        // проверка выхода за границы
+        if (playerPosition.x < -boundary)
+        {
+            transform.position = new Vector3(-boundary, playerPosition.y, playerPosition.z);
+        }
+        if (playerPosition.x > boundary)
+        {
+            transform.position = new Vector3(boundary, playerPosition.y, playerPosition.z);
+        }
+    }
+}
